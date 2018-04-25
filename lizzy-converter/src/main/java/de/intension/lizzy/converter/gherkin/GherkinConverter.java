01fc738ed38dfc661969d267789df13969989f4f
Ingo Kuba <ikuba@intension.de>
2018-04-25 09:14:38 +0200

    LIZZY-4: Convert simple gherkin text to test class code.

---
+++ lizzy-converter/src/main/java/de/intension/lizzy/converter/gherkin/GherkinConverter.java
@@ -0,0 +1,83 @@
+package de.intension.lizzy.converter.gherkin;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.squareup.javapoet.JavaFile;
+
+import de.intension.lizzy.converter.CaseFormat;
+import gherkin.ast.Feature;
+
+/**
+ * Converts a gherkin feature string to a java class file.
+ * See more about the file generation at: {@link GherkinGenerator}.
+ */
+public class GherkinConverter
+{
+
+    private String     location     = "src/test/java";
+    private String     packageName;
+    private CaseFormat methodFormat = CaseFormat.UNDERSCORE_CASE;
+
+    /**
+     * Converts a gherkin string to a java test class.
+     * Uses {@link GherkinParser#parseFeature(String)}
+     * And {@link GherkinGenerator#generate(Feature, String, CaseFormat)}
+     * to generate the file content.
+     *
+     * @param string Gherkin feature string to convert.
+     * @throws IOException When the file could not be written to.
+     */
+    public void convert(String string)
+        throws IOException
+    {
+        Feature feature = GherkinParser.parseFeature(string);
+        JavaFile javaFile = GherkinGenerator.generate(feature, packageName, methodFormat);
+        File file = new File(location);
+        javaFile.writeTo(file);
+    }
+
+    /**
+     * Determines where the java file is located.
+     * Default is src/test/java/&#60;packageName>
+     *
+     * @see #setPackageName(String)
+     */
+    public GherkinConverter setLocation(String location)
+    {
+        this.location = location;
+        return this;
+    }
+
+    /**
+     * Determines the package name of the class to generate and where the java file is located.
+     *
+     * @see #setLocation(String)
+     */
+    public GherkinConverter setPackageName(String packageName)
+    {
+        this.packageName = packageName;
+        return this;
+    }
+
+    /**
+     * @see #setMethodFormat(CaseFormat)
+     */
+    public GherkinConverter setMethodFormat(String methodFormat)
+    {
+        this.methodFormat = CaseFormat.valueOf(methodFormat);
+        return this;
+    }
+
+    /**
+     * Use this format to determine whether the method names
+     * should be generated in underscore or camel case.
+     *
+     * @see CaseFormat
+     */
+    public GherkinConverter setMethodFormat(CaseFormat methodFormat)
+    {
+        this.methodFormat = methodFormat;
+        return this;
+    }
+}
---
+++ lizzy-converter/src/test/java/de/intension/lizzy/converter/gherkin/GherkinConverterTest.java
@@ -0,0 +1,133 @@
+package de.intension.lizzy.converter.gherkin;
+
+import static de.intension.lizzy.converter.CaseFormat.UNDERSCORE_CASE;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeSpec;
+
+import gherkin.ast.Feature;
+
+@RunWith(PowerMockRunner.class)
+public class GherkinConverterTest
+{
+
+    private static final String TEST_CLASS_PATH = "src/test/java/de/intension/lizzy/converter/gherkin/TestClassName.java";
+
+    /**
+     * Given A valid input string for the parser
+     * When Converter is executed
+     * Then Parser is called with the same string argument
+     */
+    @Test
+    @PrepareForTest({GherkinParser.class})
+    public void should_use_parser()
+        throws Exception
+    {
+        // given
+        String input = "test success";
+        aParser(input);
+        try {
+            // when
+            new GherkinConverter().convert(input);
+        } catch (RuntimeException ex) {
+            // then
+            assertThat(ex.getMessage(), equalTo(input));
+        }
+    }
+
+    /**
+     * Given A parser returning valid output
+     * When Converter is executed
+     * Then Generator is called with the output from the parser
+     */
+    @Test
+    @PrepareForTest({GherkinParser.class, GherkinGenerator.class})
+    public void should_use_generator()
+        throws Exception
+    {
+        // given
+        String input = "test success";
+        aGenerator(input);
+        try {
+            // when
+            new GherkinConverter().convert(input);
+        } catch (RuntimeException ex) {
+            // then
+            assertThat(ex.getMessage(), equalTo(input));
+        }
+    }
+
+    /**
+     * Given Generator returning valid java file object
+     * When Converter is executed
+     * Then Java file is created
+     */
+    @Test
+    @PrepareForTest({GherkinParser.class, GherkinGenerator.class})
+    public void should_create_java_file_from_generator_output()
+        throws Exception
+    {
+        // given
+        aGenerator();
+        // when
+        new GherkinConverter().convert(null);
+        // then
+        File file = new File(TEST_CLASS_PATH);
+        assertThat(file.exists() && !file.isDirectory(), equalTo(true));
+    }
+
+    /**
+     * Mocks a {@link GherkinGenerator} to return a createable java file object.
+     */
+    private void aGenerator()
+    {
+        Feature feature = Mockito.mock(Feature.class);
+        PowerMockito.mockStatic(GherkinParser.class);
+        PowerMockito.when(GherkinParser.parseFeature(null)).thenReturn(feature);
+        PowerMockito.mockStatic(GherkinGenerator.class);
+        JavaFile javaFile = JavaFile.builder(this.getClass().getPackage().getName(), TypeSpec.classBuilder("TestClassName").build()).build();
+        PowerMockito.when(GherkinGenerator.generate(feature, null, UNDERSCORE_CASE)).thenReturn(javaFile);
+    }
+
+    /**
+     * Mocks a {@link GherkinGenerator} to throw a {@link RuntimeException} with a test message.
+     */
+    private void aGenerator(String string)
+    {
+        Feature feature = Mockito.mock(Feature.class);
+        PowerMockito.mockStatic(GherkinParser.class);
+        PowerMockito.when(GherkinParser.parseFeature(string)).thenReturn(feature);
+        PowerMockito.mockStatic(GherkinGenerator.class);
+        PowerMockito.when(GherkinGenerator.generate(feature, null, UNDERSCORE_CASE)).thenThrow(new RuntimeException(string));
+    }
+
+    /**
+     * Mocks a {@link GherkinParser} to throw a {@link RuntimeException} with a test message.
+     */
+    private void aParser(String string)
+    {
+        PowerMockito.mockStatic(GherkinParser.class);
+        PowerMockito.when(GherkinParser.parseFeature(string)).thenThrow(new RuntimeException(string));
+    }
+
+    @After
+    public void cleanup()
+    {
+        File file = new File(TEST_CLASS_PATH);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+}
