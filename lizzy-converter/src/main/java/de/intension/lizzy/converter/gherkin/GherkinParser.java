d7e3b9a3d59b52a983fb120a003539e7088d5764
Ingo Kuba <ikuba@intension.de>
2018-08-21 11:15:12 +0200

    LIZZY-31: Indentation in gherkin produces invalid feature.

--- lizzy-converter/src/main/java/de/intension/lizzy/converter/gherkin/GherkinParser.java
+++ lizzy-converter/src/main/java/de/intension/lizzy/converter/gherkin/GherkinParser.java
@@ -25,7 +25,66 @@ public class GherkinParser
     {
         Parser<GherkinDocument> parser = new Parser<>(new AstBuilder());
         TokenMatcher matcher = new TokenMatcher();
-        Feature feature = parser.parse(gherkin, matcher).getFeature();
+        Feature feature = parser.parse(prepare(gherkin), matcher).getFeature();
         return feature;
+    }
+
+    /**
+     * Removes leading and trailing whitespaces from all lines.
+     * 
+     * @param string Multi-line text
+     * @return Formatted string
+     */
+    private static String prepare(String string)
+    {
+        String[] lines = string.split("\n");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < lines.length; i++) {
+            String trimmed = trim(lines[i]);
+            if (!trimmed.isEmpty()) {
+                result.append(trimmed + (i == lines.length - 1 ? "" : "\n"));
+            }
+        }
+        return result.toString();
+    }
+
+    /**
+     * Removes leading and trailing whitespaces and tabulation.
+     * 
+     * @return Formatted string
+     */
+    private static String trim(String string)
+    {
+        char[] chars = string.toCharArray();
+        int start = 0;
+        // iterate through string from the start
+        for (char c : chars) {
+            if (isWhitespace(c)) {
+                start++;
+            }
+            else {
+                break;
+            }
+        }
+        int end = string.length();
+        // iterate through string from the end
+        for (int i = chars.length - 1; i > 0; i--) {
+            char c = chars[i];
+            if (isWhitespace(c)) {
+                end--;
+            }
+            else {
+                break;
+            }
+        }
+        return string.substring(start, end);
+    }
+
+    /**
+     * Matches any whitespace including non-breaking and tabulation.
+     */
+    private static boolean isWhitespace(char c)
+    {
+        return (int)c < 33 || (int)c > 126;
     }
 }
