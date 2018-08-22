e0ade09261ae5b3b7e7b5863fe8b9b6b90d9bf8f
Ingo Kuba <ikuba@intension.de>
2018-08-22 11:42:00 +0200

    LIZZY-28: Add copyright headers and author tag to java classes.

--- lizzy-adapter/src/main/java/de/intension/lizzy/adapter/jira/JiraAdapter.java
+++ lizzy-adapter/src/main/java/de/intension/lizzy/adapter/jira/JiraAdapter.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2018 Intension GmbH (https://www.intension.de)
+ * and other contributors as indicated by the @author tags.
+ * 
+ * Licensed under the Eclipse Public License - v 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.intension.lizzy.adapter.jira;
 
 import java.net.URI;
@@ -24,6 +40,8 @@ import de.intension.lizzy.adapter.MultilineTrimmer;
  * <pre>
  * <code>new JiraAdapter("https://jira.atlassian.com/", "admin", "Password123!").getIssue("INT-42")</code>
  * </pre>
+ * 
+ * @author <a href="mailto:ikuba@intension.de">Ingo Kuba</a>
  */
 public class JiraAdapter
 {
--- lizzy-converter/src/main/java/de/intension/lizzy/converter/gherkin/GherkinGenerator.java
+++ lizzy-converter/src/main/java/de/intension/lizzy/converter/gherkin/GherkinGenerator.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2018 Intension GmbH (https://www.intension.de)
+ * and other contributors as indicated by the @author tags.
+ * 
+ * Licensed under the Eclipse Public License - v 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.intension.lizzy.converter.gherkin;
 
 import java.util.function.Consumer;
@@ -71,6 +87,7 @@ import gherkin.ast.Step;
  * </pre>
  *
  * @see https://github.com/square/javapoet
+ * @author <a href="mailto:ikuba@intension.de">Ingo Kuba</a>
  */
 public class GherkinGenerator
 {
