56533a01f15098a5230a766f4e550985e252b661
Ingo Kuba <ikuba@intension.de>
2018-10-01 09:27:29 +0200

    23: Deploy updatesite additively.

--- lizzy-eclipse/lizzy-eclipse-updatesite/deployUpdatesite.sh
+++ lizzy-eclipse/lizzy-eclipse-updatesite/deployUpdatesite.sh
@@ -5,13 +5,36 @@ unzip latest/artifacts.jar -d latest/
 unzip latest/content.jar -d latest/
 rm latest/artifacts.jar
 rm latest/content.jar
+# get previous version:
 # login to intension server host:
 ncftp -u $1 -p $2 wp12720414.server-he.de <<END_SCRIPT
-# delete old jars:
-delete latest/features/*
-delete latest/plugins/*
-# upload folder to web:
-mput -r latest/
+get latest/release-number.txt
 quit
 END_SCRIPT
+mv release-number.txt latest/
+prev=$(cat latest/release-number.txt)
+echo "Previous release number: $prev"
+date=$(date +%Y%m%d.%H%M%S)
+echo "New release number: $date"
+# write new release number to file:
+echo $date > latest/release-number.txt
+if [ $3 = true ]
+then
+	echo "Deploying bugfix"
+	# upload to intension server (overwrite):
+	ncftp -u $1 -p $2 wp12720414.server-he.de <<END_SCRIPT
+	delete latest/features/*
+	delete latest/plugins/*
+	mput -r latest/
+	quit
+END_SCRIPT
+else
+	echo "Deploying release"
+	# upload to intension server (additive):
+	ncftp -u $1 -p $2 wp12720414.server-he.de <<END_SCRIPT
+	mv latest/ release-$prev/
+	mput -r latest/
+	quit
+END_SCRIPT
+fi
 echo done
